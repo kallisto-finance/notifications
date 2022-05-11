50908ce27fe697a3fe0c4a048d2a28f93a448388
Jason Jacobs <nerfnerd@gmail.com>
2022-05-11 09:39:14 -0700

    process claim liquidations

--- index.js
+++ index.js
@@ -71,11 +71,11 @@ async function send_message(message) {
     }
 }
 
-async function insert_last_block(block_id) {
+async function insert_last_block(block_id, type) {
     let result = 0;
     const query = `
-    insert into "last_notifications" (id)
-    VALUES (${block_id})
+    insert into "last_notifications" (id, type)
+    VALUES (${block_id}, ${type})
     `;
 
     try {
@@ -91,10 +91,10 @@ async function insert_last_block(block_id) {
     return result;
 }
 
-async function get_last_block() {
+async function get_last_block(block_type) {
     let result = 0;
     const query = `
-    SELECT id as block FROM "last_notifications" order by id desc
+    SELECT block FROM "last_notifications"  where type='${block_type}' order by id desc
     LIMIT 1
     `;
 
@@ -103,6 +103,30 @@ async function get_last_block() {
 
         if(res.rowCount > 0) {
             result = res.rows[0].block;
+        }
+    } catch (err) {
+        console.log(err.stack);
+    }
+
+    return result;
+}
+
+async function get_claim_liquidations(block_height) {
+    let result = null;
+    const query = `
+    select * from account_tx 
+    inner join tx on account_tx.tx_id = tx.id 
+    inner join block on tx.block_id = block.id
+    where account_tx.account = 'terra1zlntjl23grjze8k5na7fpph0whkja4zna8r3s0'
+    and data::text like '%claim_liquidation%'
+    and block.height > ${block_height} ;
+    `;
+
+    try {
+        const res = await pg_client.query(query);
+
+        if(res.rowCount > 0) {
+            result = res.rows;
         }
     } catch (err) {
         console.log(err.stack);
@@ -141,23 +165,50 @@ async function make_notifications(transaction) {
     //console.log(transaction);
 }
 
-async function main() {
+async function claim_notifications() {
     try {
-        let block = await get_last_block();
+        let block = await get_last_block('claim');
+        let transactions = await get_claim_transactions(block);
+        let max_block_height = 0;
+
+        for await (let transaction of transactions) {
+            send_message(`https://finder.terra.money/mainnet/tx/${transaction.hash}`);
+
+            if (transaction.height > max_block_height) { max_block_height = transaction.height }
+        }
+
+        //await insert_last_block(max_block_height, 'claim');
+    } catch (err) {
+        console.log(err.stack);
+    }
+
+    return null;
+}
+
+async function liquidate_notifications() {
+    try {
+        let block = await get_last_block('liquidate');
         let transactions = await get_transactions(block);
         let max_block_height = 0;
 
         for await (let transaction of transactions) {
-            await make_notifications(transaction);
+            //await make_notifications(transaction);
+            send_message(`https://finder.terra.money/mainnet/tx/${transaction.hash}`);
+
             if (transaction.height > max_block_height) { max_block_height = transaction.height }
         }
 
-        //await insert_last_block(max_block_height);
+        //await insert_last_block(max_block_height, 'liquidate');
     } catch (err) {
-         console.log(err.stack);
+        console.log(err.stack);
     }
 
     return null;
+}
+
+async function main() {
+    await liquidate_notifications;
+
 }
 
 await main();
